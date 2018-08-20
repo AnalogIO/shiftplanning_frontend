0d24d58e9fd48a0d844ec7a7f31884c423b1c7ca
xthedeus <fhov@itu.dk>
2018-08-20 14:36:50 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { 
+  AuthGuardService as AuthGuard, AuthGuardService 
+} from './auth-guard.service';
+import { ShiftsComponent } from './shifts/shifts.component';
+import { AuthService } from './auth.service';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'shifts', component: ShiftsComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: 'shifts', pathMatch: 'full'},
+  { path: '**', redirectTo: 'shifts' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ShiftsComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthGuardService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
